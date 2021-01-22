bf65b465abca1229e16255c5e411377a6e9add7a
Vinícius Viterbo <viniciusvviterbo@protonmail.com>
2021-01-22 10:34:52 -0300

    Changes on object detection search

--- people_counter.py
+++ people_counter.py
@@ -58,7 +58,7 @@ writer = None # Inicializa o gravador de vídeo - que será inicializado posteri
 W = None
 H = None
 
-ct = CentroidTracker(maxDisappeared=40, maxDistance=50) # Inicializa uma instância do rastreador de centróides, considerando um objeto não detectado por mais de 40 quadros como desaparecido 
+ct = CentroidTracker(maxDisappeared=25, maxDistance=50) # Inicializa uma instância do rastreador de centróides, considerando um objeto não detectado por mais de 20 quadros como desaparecido 
 trackers = [] # Inicializa a lista de armazenamento de cada um dos rastreadores de correlação dlib
 trackableObjects = {} # Inicializa um dictionary para registrar cada ID de um objeto com uma instância da classe TrackableObject
 
@@ -85,8 +85,7 @@ while True:
 	# Se foi informada a vontade do usuário de registrar o resultado, inicializa o gravador de vídeo com o caminho desejado
 	if args["output"] is not None and writer is None:
 		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = cv2.VideoWriter(args["output"], fourcc, 30,
-			(W, H), True)
+		writer = cv2.VideoWriter(args["output"], fourcc, 30, (W, H), True)
 
 	status = "Esperando" # Inicializa o status atual do processo
 	rects = [] # Inicializa uma lista de armazenamento dos retângulos delimitadores retornados pelo detector de objetos ou os rastreadores de correlação
@@ -109,22 +108,12 @@ while True:
 				idx = int(detections[0, 0, i, 1]) # Extrai o index da classificação dentre a lista de detecções
 
 				# Ignora a classificação se ela não seguir os objetos procurados
-				#if CLASSES[idx] != "bottle" or CLASSES[idx] != "sofa":
-				if CLASSES[idx] != "person":
+				if CLASSES[idx] not in ["chair", "sofa"]:
 					continue
 
 				# Calcula as coordenadas (x, y) da caixa delimitadora do objeto
 				box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
 				(startX, startY, endX, endY) = box.astype("int")
-
-				'''
-				# construct a dlib rectangle object from the bounding
-				# box coordinates and then start the dlib correlation
-				# tracker
-				tracker = dlib.correlation_tracker()
-				rect = dlib.rectangle(startX, startY, endX, endY)
-				tracker.start_track(rgb, rect)
-				'''
 
 				rect = dlib.rectangle(startX, startY, endX, endY) # Monta um objeto retangular da biblioteca dlib para delimitação das coordenadas
 				# Inicia o rastreador de correçações do dlib
@@ -137,8 +126,6 @@ while True:
 	else:
 		# Executo o loop para cada rastreador
 		for tracker in trackers:
-			# set the status of our system to be 'tracking' rather
-			# than 'waiting' or 'detecting'
 			status = "Tracking" # Atualiza o status do processo para a fase de rastreamento de objetos no quadro
 
 			tracker.update(rgb) # Atualiza o rastreador
@@ -152,13 +139,6 @@ while True:
 
 			rects.append((startX, startY, endX, endY)) # Informa tais posições na lista de retângulos delimitadores
 
-	#'''
-	## draw a horizontal line in the center of the frame -- once an
-	## object crosses this line we will determine whether they were
-	## moving 'up' or 'down'
-	#cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
-	#'''
-
 	# Usa do rastreador de centróides para associar o antigo objeto de centróides com  os objetos recém-computados
 	objects = ct.update(rects)
 
@@ -171,33 +151,6 @@ while True:
 		if to is None:
 			to = TrackableObject(objectID, centroid)
 
-		#'''
-		## otherwise, there is a trackable object so we can utilize it
-		## to determine direction
-		#else:
-		#	# the difference between the y-coordinate of the *current*
-		#	# centroid and the mean of *previous* centroids will tell
-		#	# us in which direction the object is moving (negative for
-		#	# 'up' and positive for 'down')
-		#	y = [c[1] for c in to.centroids]
-		#	direction = centroid[1] - np.mean(y)
-		#	to.centroids.append(centroid)
-		#	# check to see if the object has been counted or not
-		#	if not to.counted:
-		#		# if the direction is negative (indicating the object
-		#		# is moving up) AND the centroid is above the center
-		#		# line, count the object
-		#		if direction < 0 and centroid[1] < H // 2:
-		#			totalUp += 1
-		#			to.counted = True
-		#		# if the direction is positive (indicating the object
-		#		# is moving down) AND the centroid is below the
-		#		# center line, count the object
-		#		elif direction > 0 and centroid[1] > H // 2:
-		#			totalDown += 1
-		#			to.counted = True
-		#'''
-
 		trackableObjects[objectID] = to # Registra o objeto rastreável no dictionary
 
 		# Desenha as informações do objeto no quadro a ser exibido
@@ -207,12 +160,7 @@ while True:
 
 	# Monta uma lista de tuplas de informações que serão exibidas no quadro
 	info = [
-		("Contados", len(trackableObjects))
-		#'''
-		#("Up", totalUp),
-		#("Down", totalDown),
-		#("Status", status),
-		#'''
+		("Contados", len(trackableObjects)),
 	]
 
 	# Executa o loop para cada tupla de informação e as desenha no quadro
@@ -240,7 +188,7 @@ fps.stop() # Interrompe o estimador da taxa de transferência de quadros
 # Exibe as informações
 print("[INFO] Tempo decorrido: {:.2f}".format(fps.elapsed()))
 print("[INFO] FPS aproximado: {:.2f}".format(fps.fps()))
-print('[INFO] Objetos contados: {:.2f}'.format(str(len(trackableObjects))))
+print("[INFO] Objetos contados: {:.2f}".format(len(trackableObjects)))
 
 # Verifica se é necessário liberar o ponteiro do gravador de vídeo
 if writer is not None:
